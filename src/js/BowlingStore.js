7f85a232d4f235abff7eada1591b0c1b2b143a55
Giorgio <giorgio@giorgios-mbp.localdomain>
2018-03-18 00:50:38 +0100

    started adding logic for game

--- src/js/BowlingStore.js
+++ src/js/BowlingStore.js
@@ -1,9 +1,15 @@
-import { autorun, observable } from "mobx"
+import { computed, observable } from "mobx"
 
 class BowlingStore{
     @observable frames  = 10
-    @observable score   = 0
-    @observable players = 1
+    @observable players = 2
+    @observable playersArray = []
+    @observable lastScore = 0
+
+    // @computed multiPlayerFrames = (players)=>{
+    //     playersArray.length = players
+    //     return playersArray
+    // }
 }
 
 var store =  new BowlingStore
--- src/js/Game.js
+++ src/js/Game.js
@@ -3,26 +3,38 @@ import {observer} from 'mobx-react'
 
 @observer
 export default class Game extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1 className="ma0 pa5 bg-near-black tc white">
-          Babbel bowling
-          </h1>
-          <div className="mw8 center pt4">
-            <div className="pv6 ba ">
-            <p>frames: {this.props.store.frames}</p>
-            </div>
-            <div className="cf">
-              <div className="fl w-50 pa3">
-                <button className="dim bg-orange white pv3 w-100 bn br3 ">Shoot!</button>
-              </div>  
-              <div className="fl w-50 pa3">
-                <button className="dim bg-orange white pv3 w-100 bn br3 ">Shoot!</button>
-              </div>  
-            </div>
-          </div>
-        </div>
-      )
+
+  roll(){
+    if( this.props.store.frames <= 0 ){
+      alert('No frames!')
+    } else{
+      this.props.store.lastScore = Math.floor(Math.random()*10)
     }
   }
+
+  render() {
+    const {randomvals, lastScore, frames}  = this.props.store
+    return (
+      <div>
+        <h1 className="ma0 pa5 bg-near-black tc white">
+        Babbel bowling
+        </h1>
+        <div className="mw8 center pt4">
+          <div className="pv6 ba ">
+          {/* <p>Players Array: {playersArray}</p> */}
+          <p>Remaining frames: {frames}</p>
+          <p>Last Score: {lastScore}</p>
+          </div>
+          <div className="cf">
+            <div className="fl w-50 pa3">
+              <button onClick={this.roll.bind(this)} className="dim bg-orange white pv3 w-100 bn br3 ">Shoot!</button>
+            </div>  
+            <div className="fl w-50 pa3">
+              <button className="dim bg-orange white pv3 w-100 bn br3 ">Shoot!</button>
+            </div>  
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
